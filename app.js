cc4368f910b3e632e17143521b20d74e96d1dab5
Ambriel-Lourents <112871854+Ambriel-Lourents@users.noreply.github.com>
2022-10-06 14:44:36 +0200

    opdracht 6.4
    jesus

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-function kleur_Aanpassen(){
-    document.getElementById('H1txt').style.color = 'green'
+function calculate(num1, num2){
+    let answer = num1 + num2 ;
+    console.log (answer)
 }
-kleur_Aanpassen()
+calculate (42,69)
+
+function multiplyByFive(num){
+    let answer= num * 5;
+    console.log (answer)
+}
+multiplyByFive (1)
+
+function calculateminutes(nums){
+    let answer = nums / 60;
+    console.log (answer)
+}
+calculateminutes (3600)
