f3b31256012980d3b8996f7090de0a739f8a7d74
ambriel-lourents <9019110@student.zadkine.nl>
2022-11-02 09:54:00 +0100

    5.1

---
+++ p1-javascript/opdracht_5.1/app.js
@@ -0,0 +1,10 @@
+let h1 = document.getElementById("HD1") 
+let knop = document.getElementById("Button")
+function kleur1()
+{
+document.getElementById('HD1').style.color = 'red';
+}
+function kleur2()
+{
+document.getElementById('HD1').style.color = 'black';
+}
