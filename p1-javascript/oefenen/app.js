48fbef63978b384534a7535f6b2978aa20fb5f93
ambriel-lourents <9019110@student.zadkine.nl>
2022-11-02 09:38:10 +0100

    fixes

---
+++ p1-javascript/oefenen/app.js
@@ -0,0 +1,17 @@
+function calculate(num1, num2){
+    let answer = num1 + num2 ;
+    console.log (answer)
+}
+calculate (42,69)
+
+function multiplyByFive(num){
+    let answer= num * 5;
+    console.log (answer)
+}
+multiplyByFive (1)
+
+function calculateminutes(nums){
+    let answer = nums / 60;
+    console.log (answer)
+}
+calculateminutes (3600)
---
+++ p1-javascript/opdracht_7.2/app.js
@@ -0,0 +1,12 @@
+let kost = prompt("Hoeveel kost het/");
+let budget = 250;
+let product = kost;
+if(product > budget){
+console.log("Helaas heb je niet genoeg brokie")
+}
+else if (product = budget){
+    console.log("Genoeg")
+}
+else{
+    console.log("U heeft voldoende saldo")
+}
